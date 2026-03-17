e7606d62bd0bbd6cbe6ecf800812978d914c2880
MAJAYGH8374 <129788329+MAJAYGH8374@users.noreply.github.com>
2026-03-17 21:50:03 +0530

    Create common.groovy

---
+++ common.groovy
@@ -0,0 +1,14 @@
+def call(string stageName)
+{
+  if("${stageName}" == "build")
+  {
+    sh "mvn clean package"
+  }
+else if("${stageName}" == "sonarqube")
+  {
+    sh "mvn clean sonar:sonar"
+  }
+ else if("${stageName}" == "nexus")
+  {
+    sh "mvn sonar:sonar deploy"
+  }
